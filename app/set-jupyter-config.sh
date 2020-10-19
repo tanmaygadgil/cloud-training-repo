7ce84eabd6878ac597a2818793f52cc200e5f336
tanmay-p-gadgil <tanmay-p-gadgil@pwc.com>
2020-10-19 17:36:19 +0530

    docker added

---
+++ app/set-jupyter-config.sh
@@ -0,0 +1,9 @@
+jupyter notebook --generate-config
+
+printf "c = get_config()
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888" >> ~/.jupyter/jupyter_notebook_config.py
+
+
+
